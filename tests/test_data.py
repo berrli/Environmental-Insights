a26f052d631d38a1562fdb925e0a111283033017
Liam Berrisford <liam.j.berrisford@bath.edu>
2024-08-07 19:23:44 +0100

    Refactor tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 # Add the parent directory to the system path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../environmental_insights')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import environmental_insights.air_pollution_functions as ei_air_pollution_functions
 import environmental_insights.data as ei_data
--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 # Add the parent directory to the system path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../environmental_insights')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import sys
 import os
